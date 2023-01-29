eef508657e53c9330afb9807f3c33b0c0b8c5d92
olha0108 <ol0108ha@gmail.com>
2023-01-29 13:07:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ window.onscroll = () => {
   //Якщо прокручено більше 700, кнопка з'являється
   if (window.scrollY > 700) {
     btnScroll.classList.remove('to-top--hide');
-    //Якщо прокручено менше 700, кнопка зникає
+  //Якщо прокручено менше 700, кнопка зникає
   } else if (window.scrollY < 700) {
     btnScroll.classList.add('to-top--hide');
   }
