b874a0125d2f142787c94a1fa4f756f880971341
olha0108 <101519540+olha0108@users.noreply.github.com>
2023-01-29 11:09:31 +0000

    Deploying to gh-pages from @ olha0108/donut@eef508657e53c9330afb9807f3c33b0c0b8c5d92 🚀

---
+++ index.649ae25b.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".swiper",{slideClass:"swiper__slide",wrapperClass:"swiper__wrapper",pagination:{el:".swiper__pagination",type:"fraction"},autoplay:{delay:3e3},allowTouchMove:!1,spaceBetween:32,initialSlide:1,centeredSlides:!0,loop:!0,speed:500,effect:"slide",breakpoints:{320:{slidesPerView:1},768:{slidesPerView:3}}});
+//# sourceMappingURL=index.649ae25b.js.map
